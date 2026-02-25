5b8e9fcb73a19e732aab2554281cce0fe65fbea6
felipeha323 <60258356+felipeha323@users.noreply.github.com>
2026-02-25 16:50:56 -0500

    creation integration Test Delete

--- base/src/test/java/com/example/base/user/infrastructure/Integration/DeleteUserUseCaseIntegrationTest.java
+++ base/src/test/java/com/example/base/user/infrastructure/Integration/DeleteUserUseCaseIntegrationTest.java
@@ -4,6 +4,7 @@ package com.example.base.user.infrastructure.Integration;
 import com.example.base.user.application.dto.CreateUserDTO;
 import com.example.base.user.application.dto.UserResponseDTO;
 import com.example.base.user.application.useCases.CreateUserUseCase;
+import com.example.base.user.application.useCases.DeleteUserUseCase;
 import com.example.base.user.infrastructure.persistence.JpaUserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,14 +18,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class CreateUserUseCaseIntegrationTest {
+public class DeleteUserUseCaseIntegrationTest {
 
     @Autowired
     private CreateUserUseCase createUserUseCase;
 
     @Autowired
+    private DeleteUserUseCase deleteUserUseCase;
+
+    @Autowired
     private JpaUserRepository jpaUserRepository;
 
+    private Long userIdToDelete;
 
     @BeforeEach
     void setUp() {
@@ -33,28 +38,19 @@ public class CreateUserUseCaseIntegrationTest {
 
     @Test
     @DisplayName("integration succes:  UseCase -> Adapter -> DB")
-    void integrationCreateUser() {
+    void  IntegrationdeleteUser() {
+        // Crear un usuario para luego eliminarlo
+        CreateUserDTO createDTO = new CreateUserDTO("TestUser", "test@mail.com", "password123", new Long[]{1L});
+        UserResponseDTO createdUser = createUserUseCase.create(createDTO);
+        userIdToDelete = createdUser.getId();
 
-        CreateUserDTO userRequest = new CreateUserDTO( "Red", "red@pallet.com", "password123", new Long[]{1L, 2L});
+        deleteUserUseCase.delete(userIdToDelete);
 
-        UserResponseDTO savedUser = createUserUseCase.create(userRequest);
-
-        // Validamos la respuesta del dominio
-        assertThat(savedUser.getId()).isNotNull();
-        assertThat(savedUser.getUsername()).isEqualTo("Red");
-
-        // Verificación de Infraestructura: ¿Está realmente en PostgreSQL?
-        var userInDb = jpaUserRepository.findById(savedUser.getId());
-        assertThat(userInDb).isPresent();
-        assertThat(userInDb.get().getPokemonsIds()).containsExactly(1L, 2L);
-        assertThat(userInDb.get().getEmail()).isEqualTo("red@pallet.com");
-
-
+        // Verificar que el usuario ya no existe en la base de datos
+        boolean exists = jpaUserRepository.existsById(userIdToDelete);
+        assertThat(exists).isFalse();
 
 
     }
-
-
-
 
 }
